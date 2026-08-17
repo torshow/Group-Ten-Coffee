35b51a0777971a24777d2aa0e5836400ac07e1dc
Briannicks <your-email@example.com>
2026-08-17 10:43:06 +0300

    redeployment

--- client/src/Components/NavBar.jsx
+++ client/src/Components/NavBar.jsx
@@ -1,19 +1,29 @@
 import { NavLink } from 'react-router-dom'
 
-function Navbar() {
+function NavBar() {
   return (
     <nav className="navbar">
-      <NavLink to="/" className="nav-link" end>
+      <NavLink
+        to="/"
+        end
+        className={({ isActive }) => (isActive ? 'nav-link active' : 'nav-link')}
+      >
         Home
       </NavLink>
-      <NavLink to="/shop" className="nav-link">
+      <NavLink
+        to="/shop"
+        className={({ isActive }) => (isActive ? 'nav-link active' : 'nav-link')}
+      >
         Shop
       </NavLink>
-      <NavLink to="/admin" className="nav-link">
+      <NavLink
+        to="/admin"
+        className={({ isActive }) => (isActive ? 'nav-link active' : 'nav-link')}
+      >
         Admin Portal
       </NavLink>
     </nav>
   )
 }
 
-export default Navbar;
+export default NavBar
